41d40c8373162d8e1b123d341c9d8b29bc0acdc8
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-05-18 08:56:41 +0200

    set network category of client to private

--- Powershell w2k16/7-InitialConfigureCliente.sh
+++ Powershell w2k16/7-InitialConfigureCliente.sh
@@ -6,6 +6,8 @@ Rename-Computer -NewName $ClientName -force -Confirm:$False
 
 netsh interface ipv4 set address name="Ethernet0" static 192.168.1.101 255.255.255.0 
 
+Set-NetConnectionProfile -InterfaceAlias Ethernet0 -NetworkCategory Private
+
 #Permitir el acceso remoto al no estar dentro del dominio.
 Get-NetConnectionProfile
 Enable-PSRemoting
@@ -30,6 +32,8 @@ clear
 # Dos parámetros que se pueden utilizar con Enable-PSRemoting son -Force y -Confirm.
 # -Force para omitir todas las indicaciones que Enable-PSRemoting normalmente le daría al ejecutar el comando.
 # -Confirm:$false obtendría el mismo resultado que -Forece
+
+
 
 Get-NetConnectionProfile
 Enable-PSRemoting
