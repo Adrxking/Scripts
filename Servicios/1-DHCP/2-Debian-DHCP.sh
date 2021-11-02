63271da78fa4c2e0b2a288c9a41c89e04648ec75
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-11-02 13:10:21 +0100

    cambio de carpeta

--- Servicios/1-DHCP/2-Debian-DHCP.sh
+++ Servicios/1-DHCP/2-Debian-DHCP.sh
@@ -12,40 +12,65 @@ apt install isc-dhcp-server iptables -y
 echo 'INSTALACIONES COMPLETAS'
 
 ###################################################
+#####-----Copia de DHCPd.conf.original--------#####
+###################################################
+cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.original
+
+###################################################
 #####-----Configuracion Global DHCPd.conf-----#####
 ###################################################
 echo 'COMIENDO DE LA CONFIGURACION EN /etc/dhcp/dhcpd.conf'
 
-echo ddns-update-style none; > /etc/dhcp/dhcpd.conf 
+echo 'ddns-update-style none;' > /etc/dhcp/dhcpd.conf 
 
-echo default-lease-time 6000; >> /etc/dhcp/dhcpd.conf 
-echo max-lease-time 7200; >> /etc/dhcp/dhcpd.conf 
+echo '' >> /etc/dhcp/dhcpd.conf 
 
-#####--------------LAN 10.33.6.0--------------#####
-echo subnet 10.33.6.0 netmask 255.255.255.0{ >> /etc/dhcp/dhcpd.conf 
-echo 'range 10.33.6.101 10.33.6.150;' >> /etc/dhcp/dhcpd.conf 
-echo 'option routers 10.33.6.1;' >> /etc/dhcp/dhcpd.conf 
-echo 'option domain-name-servers 8.8.8.8;' >> /etc/dhcp/dhcpd.conf 
-echo } >> /etc/dhcp/dhcpd.conf
+echo 'default-lease-time 6000;' >> /etc/dhcp/dhcpd.conf 
+echo 'max-lease-time 7200;' >> /etc/dhcp/dhcpd.conf 
 
-#####--------------LAN 191.16.6.0---------------#####
-echo subnet 191.16.6.0 netmask 255.255.255.0{ >> /etc/dhcp/dhcpd.conf 
-echo 'range 191.16.6.101 191.16.6.150;' >> /etc/dhcp/dhcpd.conf 
-echo 'option routers 191.16.6.1;' >> /etc/dhcp/dhcpd.conf 
-echo 'option domain-name-servers 8.8.4.4;' >> /etc/dhcp/dhcpd.conf 
-echo } >> /etc/dhcp/dhcpd.conf
+echo '' >> /etc/dhcp/dhcpd.conf 
+
+#####--------------LAN 192.168.2.0--------------#####
+echo 'subnet 192.168.2.0 netmask 255.255.255.0 {' >> /etc/dhcp/dhcpd.conf 
+echo '  range 192.168.2.101 192.168.2.254;' >> /etc/dhcp/dhcpd.conf 
+echo '  option routers 192.168.2.1;' >> /etc/dhcp/dhcpd.conf 
+echo '  option domain-name-servers 8.8.8.8;' >> /etc/dhcp/dhcpd.conf 
+echo '}' >> /etc/dhcp/dhcpd.conf
+
+echo '' >> /etc/dhcp/dhcpd.conf 
+
+#####--------------LAN 192.168.3.0--------------#####
+echo 'subnet 192.168.3.0 netmask 255.255.255.0 {' >> /etc/dhcp/dhcpd.conf 
+echo '  range 192.168.3.101 192.168.3.254;' >> /etc/dhcp/dhcpd.conf 
+echo '  option routers 192.168.3.2;' >> /etc/dhcp/dhcpd.conf 
+echo '  option domain-name-servers 8.8.8.8;' >> /etc/dhcp/dhcpd.conf 
+echo '}' >> /etc/dhcp/dhcpd.conf
+
+echo '' >> /etc/dhcp/dhcpd.conf 
+
+#####--------------Reserva de IPs---------------#####
+echo 'host adrian {' >> /etc/dhcp/dhcpd.conf 
+echo '  hardware ethernet 00:00:00:00:00:a5;' >> /etc/dhcp/dhcpd.conf 
+echo '  fixed-address 192.168.2.60;' >> /etc/dhcp/dhcpd.conf 
+echo '}' >> /etc/dhcp/dhcpd.conf
 
 echo 'FIN DE LA CONFIGURACION EN /etc/dhcp/dhcpd.conf'
 
 ###################################################
 ##--Configuracion /etc/default/isc-dhcp-server--###
 ###################################################
-echo 'COMIENZO DE LA CONFIGURACION EN /etc/default/isc-dhcp-server.conf'
+# * IMPORTANTE COMPROBAR CUALES SON LAS INTERFACES * #
+echo 'COMIENZO DE LA CONFIGURACION EN /etc/default/isc-dhcp-server'
 
 echo INTERFACESv4="ens33 ens38" > /etc/default/isc-dhcp-server
 echo INTERFACESv6="" >> /etc/default/isc-dhcp-server
 
-echo 'FIN DE LA CONFIGURACION EN /etc/default/isc-dhcp-server.conf'
+echo 'FIN DE LA CONFIGURACION EN /etc/default/isc-dhcp-server'
+
+###################################################
+####----Configuracion /etc/dhcp/sysctl.conf----####
+###################################################
+echo 'net.ipv4.ip_forward=1' > /etc/sysctl.conf
 
 ###################################################
 #########------IPTABLES PARA NAT-----##############
@@ -54,4 +79,9 @@ echo 'COMIENZO DE LA CONFIGURACION IPTABLES /etc/default/isc-dhcp-server.conf'
 sudo iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
 echo 'FIN DE LA CONFIGURACION IPTABLES /etc/default/isc-dhcp-server.conf'
 
-echo 'REINICIA EL PC Y LO TENDRÁS TODO LISTO, DISFRUTA!'
+###################################################
+#########--------RELEER SYSCTL-------##############
+###################################################
+sysctl -p /etc/sysctl.conf
+
+echo 'TODO LISTO, DISFRUTA!'
--- Servicios/3-DNS/Create Zones and Configuration/Lubuntu.sh
+++ Servicios/3-DNS/Create Zones and Configuration/Lubuntu.sh
@@ -18,6 +18,16 @@ echo '      file "/etc/bind/db.10.33.6"; ' >> /etc/bind/named.conf.local
 echo "      notify yes; "                  >> /etc/bind/named.conf.local # Cuando haya cambios en la zona se notifican a los secundarios que no estén declarados
 echo "};"                                  >> /etc/bind/named.conf.local
 
+echo ""                                    >> /etc/bind/named.conf.local
+
+###################################################
+#######--------Subdominio Forma 2----------########
+###################################################
+echo 'zone "bbdd.asir6" {'                    >> /etc/bind/named.conf.local
+echo "      type master;"                     >> /etc/bind/named.conf.local
+echo '      file "/etc/bind/db.bbdd.asir6"; ' >> /etc/bind/named.conf.local
+echo "};"                                     >> /etc/bind/named.conf.local
+
 
 ###################################################
 ###---Crear configuracion de la DB de las zonas--##
@@ -45,13 +55,21 @@ echo "ns3      IN  A        10.33.6.2"      >> /etc/bind/db.asir6 # Zona secunda
 echo "wserver  IN  A        10.33.6.4"      >> /etc/bind/db.asir6
 echo "@        IN  MX 10    debian.asir6."  >> /etc/bind/db.asir6 # MX es del mail y el 10 es la prioridad, a menor número mayor prioridad
 echo "@        IN  MX 20    wserver.asir6." >> /etc/bind/db.asir6
+echo "equipo1.redes        IN  A    10.33.6.101" >> /etc/bind/db.asir6 # Subdominio con 2 equipos
+echo "equipo2.redes        IN  A    10.33.6.102" >> /etc/bind/db.asir6 # Subdominio con 2 equipos
+###################################################
+#######--------Subdominio Forma 1----------########
+###################################################
+#echo "equipo1.redes        IN  A    10.33.6.101" >> /etc/bind/db.asir6 # Subdominio con 2 equipos
+#echo "equipo2.redes        IN  A    10.33.6.102" >> /etc/bind/db.asir6 # Subdominio con 2 equipos
 
+# * ZONA INVERSA * #
 touch /etc/bind/db.10.33.6
 
 echo ";"                                               > /etc/bind/db.10.33.6 
 echo '"$TTL"    86400'                                 >> /etc/bind/db.10.33.6
 echo "@     IN  SOA  ns1.asir6.  adrian.asir6.  ("     >> /etc/bind/db.10.33.6
-echo "                       3   ; Serial"             >> /etc/bind/db.10.33.6
+echo "                       4   ; Serial"             >> /etc/bind/db.10.33.6
 echo "                  604800   ; Refresh"            >> /etc/bind/db.10.33.6
 echo "                   86400   ; Retry"              >> /etc/bind/db.10.33.6 
 echo "                 2419200   ; Expire"             >> /etc/bind/db.10.33.6
@@ -61,9 +79,31 @@ echo "@        IN  NS       ns1.asir6."     >> /etc/bind/db.10.33.6
 #echo "@        IN  NS       ns2.asir6."     >> /etc/bind/db.10.33.6 # Zona secundaria del wserver
 echo "@        IN  NS       ns3.asir6."     >> /etc/bind/db.10.33.6 # Zona secundaria del debian
 echo "1        IN  PTR      ipfire.asir6."  >> /etc/bind/db.10.33.6
-echo "1        IN  PTR      router.asir6."  >> /etc/bind/db.10.33.6 # Este es el CNAME
-echo "2        IN  PTR      debian.asir6."  >> /etc/bind/db.10.33.6
+###################################################
+#####-----Subdominio Forma 1 y 2 INVERSA-----######
+###################################################
+echo "101      IN  PTR      equipo1.redes.asir6."  >> /etc/bind/db.10.33.6
+echo "102      IN  PTR      equipo1.redes.asir6."  >> /etc/bind/db.10.33.6
+echo "201      IN  PTR      pc01.bbdd.asir6."  >> /etc/bind/db.10.33.6
+echo "202      IN  PTR      pc02.bbdd.asir6."  >> /etc/bind/db.10.33.6
 
+###################################################
+#######--------Subdominio Forma 2----------########
+###################################################
+touch /etc/bind/db.bbdd.asir6
+
+echo ";"                                               > /etc/bind/db.bbdd.asir6
+echo '"$TTL"    86400'                                 >> /etc/bind/db.bbdd.asir6
+echo "@     IN  SOA  ns1.asir6.  adrian.asir6.  ("     >> /etc/bind/db.bbdd.asir6 # Se podría poner ns1.bbdd.asir6. pero habría que crear un registro de tipo A
+echo "                       3   ; Serial"             >> /etc/bind/db.bbdd.asir6
+echo "                  604800   ; Refresh"            >> /etc/bind/db.bbdd.asir6
+echo "                   86400   ; Retry"              >> /etc/bind/db.bbdd.asir6 
+echo "                 2419200   ; Expire"             >> /etc/bind/db.bbdd.asir6
+echo "                   86400 ) ; Negative Cache TTL" >> /etc/bind/db.bbdd.asir6
+echo ";"                                               >> /etc/bind/db.bbdd.asir6
+echo "@     IN  NS   ns1.asir6."                       >> /etc/bind/db.bbdd.asir6
+echo "pc01  IN  A    10.33.6.201"                      >> /etc/bind/db.bbdd.asir6
+echo "pc02  IN  A    10.33.6.201"                      >> /etc/bind/db.bbdd.asir6
 
 ###################################################
 ###-----Comprobar zonas directas e inversas-----###
@@ -73,6 +113,6 @@ echo "named-checkconf checked"
 
 service bind9 restart
 
-sleep 5
+sleep 3
 
 named-checkzone asir6 /etc/bind/db.asir6
