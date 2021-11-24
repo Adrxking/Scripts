422d9e422b3973574d82c38e297b5dd1956ea0e6
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-11-24 10:50:12 +0100

    added more configuration to proftpd

--- Servicios/4-FTP/ProFTPD/Installation.sh
+++ Servicios/4-FTP/ProFTPD/Installation.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 apt install proftpd
+
+# Si el servicio no inicia debemos cargar el módulo mod_ident.c
+# echo "LoadModule mod_ident.c" >> /etc/proftpd/modules.conf
+
+###################################################
+########-------CONFIGURACIÓN PROFTPD-------########
+###################################################
+echo "<Limit Login>" >> /etc/proftpd/proftpd.conf
+echo "  AllowUser usuario" >> /etc/proftpd/proftpd.conf
+echo "  DenyAll" >> /etc/proftpd/proftpd.conf
+echo "</Limit>" >> /etc/proftpd/proftpd.conf
+
+sed -i "s/# RequireValidShelloff/RequireValidShell off/" /etc/proftpd/proftpd.conf
