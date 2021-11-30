c8c5d0ee330a43c2440b63cf32ab62e9894b0de5
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-11-30 14:26:24 +0100

    regular expressions linux

--- Manual Bash/RegularExpressions.sh
+++ Manual Bash/RegularExpressions.sh
@@ -1,5 +1,7 @@
+^     --> Representa el comienzo de una cadena
+$     --> Representa el final de una cadena
 .     --> Permite cualquier caracter
-[ ]   --> Permite ese rango de caracteres
+[ ]   --> Permite ese rango de caracteres si se expresa con (-) o esos caracteres si no lleva guión
 [^ ]  --> Permite todos los caracteres excepto los introducidos en los corchetes
 *     --> Permite 0 o + del item anterior
 +     --> Permite 1 o + del item anterior
@@ -9,6 +11,8 @@
 {n m} --> Permite entre ‘n’ y ‘m’ números del item anterior
 { ,m} --> Permite máximo ‘m’ números, m incluido del item anterior
 \     --> Permite escapar caracteres
+|     --> Permite que aparezca una opción u otra Ej: "uno|dos"
+()    --> Permite representar conjuntos Ej: "(ab)+" --> Solo muestra lo que tenga ab si o sí
 
 Ejemplo filtro de números positivos y negativos con decimales
 '^[-+]?[0-9]+\.?[0-9]*$'
