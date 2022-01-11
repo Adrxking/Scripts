2c43a1fb53a24e3e965e5555913a50051aa36ef1
Adrianas_Vitys <adrianalbujaira@gmail.com>
2022-01-11 13:02:14 +0100

    ASO Projects

--- ASO/7-Proyectos/1. AutomatizaciónUsuarios.sh
+++ ASO/7-Proyectos/1. AutomatizaciónUsuarios.sh
@@ -1,19 +1,19 @@
-# ! --> Script para crear usuarios importados desde un archivo de texto:
+# ! --> Script para crear usuarios importados desde un archivo de texto y exportar los usuarios a un fichero de texto:
 
-# * El script debe recibir como parámetro el nombre del archivo y debe comprobar que el archivo existe.
+# * El script debe recibir como parámetro el nombre del archivo y debe comprobar que el archivo existe. ✓
 
-# * En el archivo debe aparecer en cada línea el usuario, un carácter de dos puntos para separar
-# * los campos, grupo primario y comentario (Ejemplo: pepe:users:Usuario pepe).
+# * En el archivo debe aparecer en cada línea el usuario, un carácter de dos puntos para separar.
+# * los campos son: Nombre, Grupo primario y Comentario (Ejemplo: pepe:users:Usuario pepe). ✕
 
 # * Cuando creas un usuario con useradd y pones la contraseña en -p tienes que poner la contraseña cifrada
-# * mkpassword cifra textos en formato de contraseña
+# * mkpassword cifra textos en formato de contraseña. ✕
 
-# * Si la carpeta personal del usuario no existe se debe crear.
+# * Si la carpeta personal del usuario no existe se debe crear. ✓
 
-# * A los usuarios creados se les asignará la shell /bin/bash.
+# * A los usuarios creados se les asignará la shell /bin/bash. ✓
 
 # * Si el usuario se crea correctamente se guardará un fichero llamado usuarios.log en el que debe 
-# * aparecer cada usuario creado junto con su clave temporal asignada.
+# * aparecer cada usuario creado junto con su clave temporal asignada. ✓
 
 #!/bin/bash
 
@@ -31,7 +31,7 @@ then
 
         passwd=$( echo $RANDOM )
 
-        shortPasswd="${passwd:0:4}"
+        shortPasswd="${passwd:0:6}"
 
         groupadd "${group}"
 
@@ -41,7 +41,7 @@ then
          then
             echo "Usuario: ${name} --- Clave Temporal: ${shortPasswd}" >> ~/usuarios.log
         else
-            echo "Ha ocurrido un error creando el usuario"
+            echo "Ha ocurrido un error creando el usuario ${name}"
         fi
     done
 else
