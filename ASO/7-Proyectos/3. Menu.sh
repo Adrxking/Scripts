dba29d3c4ca5cb3645ebb7125650cb20fce6e7c8
Adrianas_Vitys <adrianalbujaira@gmail.com>
2022-02-03 12:42:55 +0100

    menu finalizado

--- ASO/7-Proyectos/3. Menu.sh
+++ ASO/7-Proyectos/3. Menu.sh
@@ -10,8 +10,8 @@ clear
 
 # Función que muestra las opciones del menú
 getOptions() {
-    echo "1) Backup usuarios"
-    echo "2) Restaurar usuarios"
+    echo "1) Obtener log con los recursos del sistema"
+    echo "2) Ejecutar menu de iptables"
     echo "3) Salir"
 }
 
@@ -29,17 +29,13 @@ clear
     # Comprobar que ha introducido el usuario
     case ${option} in
         1)
-            # Si el usuario introduce la opción 1 le pedimos que introduzca los usuarios
-            read -p "Introduce los nombres de los usuarios separados por espacio --> " users
-            
-            bash backup.sh $users
+            # Si el usuario introduce la opción 1 ejecutamos el script de obtener los recursos del sistema
+            bash monitoreo.sh
             
         ;;
         2)
-            # Si el usuario introduce la opción 2 le pedimos que introduzca la ruta del backup y los usuarios que restaurar
-            read -p "Introduce la ruta de los backup y los nombres de los usuarios separados por espacio --> " recover
-
-            bash recovery.sh $recover
+            # Si el usuario introduce la opción 2 le ofrecemos el menu de iptables
+            bash iptables.sh
         ;;
         *)
             # En caso de q el usuario introduzca una opción no válida se mostrará lo siguiente
