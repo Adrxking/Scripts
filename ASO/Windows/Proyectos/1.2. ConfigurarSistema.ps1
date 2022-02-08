7d37381b19c96045430913e9664e00766e018fde
Adrianas_Vitys <adrianalbujaira@gmail.com>
2022-02-08 14:23:52 +0100

    Proyecto mejorado

---
+++ ASO/Windows/Proyectos/1.2. ConfigurarSistema.ps1
@@ -0,0 +1,47 @@
+<# SCRIPT PARTE 2
+
+--> 1. Realizar la instalación y configuración inicial de un Windows Server utilizando PowerShell, 
+       teniendo en cuenta los siguientes requisitos del sistema:
+        --> 1.1 El nombre del servidor será cajal (En honor al premio nobel de medicina Santiago Ramón y Cajal)
+        --> 1.2 La red privada de la empresa es la 172.16.0.0 / 16.
+        --> 1.3 La dirección IP del servidor debe ser la 172.16.0.10
+        --> 1.4 Dar de alta a los siguientes usuarios locales pertenecientes al departamento de sistemas:
+            --> 1.4.1 Sara Castillo (Administradora del sistema)
+            --> 1.4.2 Sergio Torres (Operador de copias de seguridad)
+        --> 1.5 Crear una carpeta compartida para guardar la documentación generada por el departamento de sistemas. 
+        Sólo los miembros del departamento de sistemas tendrán acceso a esta carpeta.
+        --> 1.6 Realizar una copia de seguridad automáticamente todos los días fuera del horario laboral de la carpeta
+        con la documentación del departamento de sistemas.
+#>
+<# VARIABLES #>
+$Usuario1="SaraCastillo"
+$Usuario1Pass="P@ssword"
+$Usuario2="SergioTorres"
+$Usuario2Pass="P@ssword"
+
+<# 1.4.1 - Alta usuario Sara Castillo (Administradora del sistema) #>
+$Usuario1PassSecure=ConvertTo-SecureString $Usuario1Pass -asplaintext -force
+New-LocalUser $Usuario1 -Password $Usuario1PassSecure
+# Añadir al usuario al groupo Administradores 
+Add-LocalGroupMember -Group "Administradores" -Member $Usuario1
+Write-Output " ##### ESTOS SON LOS MIEMBROS DEL GRUPO Administradores ##### "
+Get-LocalGroupMember -Group "Administradores"
+
+<# 1.4.2 - Sergio Torres (Operador de copias de seguridad) #>
+$Usuario2SecurePass=ConvertTo-SecureString $Usuario2Pass -asplaintext -force
+New-LocalUser $Usuario2 -Password $Usuario2SecurePass
+# Añadir al usuario al groupo Operadores de copia de seguridad
+Add-LocalGroupMember -Group "Operadores de copia de seguridad" -Member $Usuario2
+Write-Output " ##### ESTOS SON LOS MIEMBROS DEL GRUPO Operadores de copia de seguridad ##### "
+Get-LocalGroupMember -Group "Operadores de copia de seguridad"
+
+<# 1.5. Compartir Carpeta para los administradores #>
+#Crear un directorio
+New-Item C:\Shared -type directory
+#Compartir un directorio
+New-SmbShare -Name SharedAdmin -Path C:\Shared -FullAccess Administradores
+#Verificar los permisos de un directorio compartido.
+Write-Output " ## ESTOS SON LOS PERMISOS DEL DIRECTORIO COMPARTIDO SharedAdmin ##"
+Get-SmbShareAccess SharedAdmin
+
+<# 1.6. Copia de seguridad diaria de la carpeta SharedAdmin #>
--- W2k16 Initial Configuration/1-InitialConfigureServer.ps1
+++ W2k16 Initial Configuration/1-InitialConfigureServer.ps1
@@ -1,73 +1,73 @@
-clear
+Clear-Host
 $DIR="c:"
 $ServerName="Server16"
 $ServerIp="192.168.1.100"
 $ServerGateway="192.168.1.1"
 
 write-host -backgroundcolor BLUE -foregroundcolor YELLOW "Configuración inicial. Implica REINICIO del Sistema cuando finalice. "
-echo " " 
+Write-Output " " 
 write-host -BackgroundColor MAGENTA "1.- Desactivación de Windows Defender. "
-echo " " 
+Write-Output " " 
 Uninstall-WindowsFeature -Name Windows-Defender
-echo " " 
+Write-Output " " 
 write-host -foregroundcolor GREEN -nonewline "    Hecho. Reinicio Pendiente. "
-echo " " 
+Write-Output " " 
 write-host -BackgroundColor MAGENTA "2.- Desactivación del UAC. Control de Cuentas de Usuario. "
-echo " " 
+Write-Output " " 
 cd HKLM:\
 $RegKey ='HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system'
 Set-ItemProperty -Path $RegKey -Name EnableLUA -Value 0 -Confirm:$False
-echo " " 
+Write-Output " " 
 write-host -foregroundcolor GREEN -nonewline "    Hecho. Reinicio Pendiente. "
 cd $Dir
-echo " " 
+Write-Output " " 
 write-host -BackgroundColor MAGENTA "3.- Desactivación del FIREWALL. "
-echo " " 
+Write-Output " " 
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-echo " " 
+Write-Output " " 
 write-host -foregroundcolor GREEN -nonewline "    Hecho. "
-echo " " 
+Write-Output " " 
 write-host -BackgroundColor MAGENTA "4.- Cambio de nombre al Servidor. "
-echo " "  
+Write-Output " "  
 Rename-Computer -NewName $ServerName -force -Confirm:$False
-echo " " 
+Write-Output " " 
 write-host -foregroundcolor GREEN -nonewline "    Hecho. Reinicio Pendiente. " 
-echo " " 
+Write-Output " " 
 write-host -BackgroundColor MAGENTA "5.- Desactivación protocolo de RED IPv6. "
-echo " " 
+Write-Output " " 
 reg add hklm\system\currentcontrolset\services\tcpip6\parameters /v DisabledComponents /t REG_DWORD /d 0xFF /f
 $TErminado=(Get-NetAdapterBinding -ComponentID "ms_tcpip6" | disable-NetAdapterBinding -ComponentID "ms_tcpip6" –PassThru) > $null
 if ( !$Terminado.Enabled )
   {
-   echo $TErminado
-   echo " " 
+   Write-Output $TErminado
+   Write-Output " " 
    write-host -foregroundcolor YELLOW -nonewline "    Hecho. Preferible Reinicio. " 
   } 
-echo " "
+Write-Output " "
 write-host -BackgroundColor MAGENTA "6.- Establecer Dirección IPv4 Estática. "
-echo " " 
+Write-Output " " 
 Set-ItemProperty -Path “HKLM:\SYSTEM\CurrentControlSet\services\Tcpip\Parameters\Interfaces\$((Get-NetAdapter -InterfaceIndex (Get-NetAdapter).InterfaceIndex ).InterfaceGuid)” -Name EnableDHCP -Value 0 -PassThru -Force -Confirm:$False > $null
 Remove-NetIpAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -AddressFamily IPv4 -Confirm:$False -PassThru > $Null
 Remove-NetRoute -InterfaceIndex (Get-NetAdapter).InterfaceIndex -AddressFamily IPv4 -Confirm:$False -PassThru > $Null
 New-NetIpAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -IpAddress $ServerIp -PrefixLength 24 -DefaultGateway $ServerGateway -AddressFamily IPv4 -Confirm:$False > $Null
 Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses $ServerIp > $Null
-echo " " 
+Write-Output " " 
 write-host -foregroundcolor YELLOW -nonewline "    Hecho. " 
-echo " "
+Write-Output " "
 write-host -backgroundcolor Blue -foregroundcolor YELLOW "   Configuración inicial. "
-echo " " 
+Write-Output " " 
 write-host -foregroundColor GREEN "    1.- Desactivación de Windows Defender."
-echo " " 
+Write-Output " " 
 write-host -ForegroundColor GREEN "    2.- Desactivación del UAC. Control de Cuentas de Usuario."
-echo " " 
+Write-Output " " 
 write-host -ForegroundColor GREEN "    3.- Desactivación del FIREWALL."
-echo " "
+Write-Output " "
 write-host -ForegroundColor GREEN "    4.- Cambio de nombre al Servidor."
-echo " "
+Write-Output " "
 write-host -ForegroundColor GREEN "    5.- Desactivación protocolo de RED IPv6."
-echo " "
+Write-Output " "
 write-host -foregroundColor GREEN "    6.- Establecer Dirección IPv4 Estática."
-echo " "
+Write-Output " "
 write-host -ForegroundColor Red "       Se procede al REINICIO DEL SISTEMA " -NoNewline
 Start-Sleep -Seconds 5
 restart-computer -Confirm:$False
