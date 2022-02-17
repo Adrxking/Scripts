14b8f46fb4c518432f089d6837e2d6f0dc68616f
Adrianas_Vitys <adrianalbujaira@gmail.com>
2022-02-17 10:29:30 +0100

    trying iptables

--- SAD/Secure Network Configuration/Firewall.sh
+++ SAD/Secure Network Configuration/Firewall.sh
@@ -10,7 +10,23 @@ apt update -y && apt upgrade -y
 ###################################################
 ###-----Instalar isc-dhcp-server e iptables-----###
 ###################################################
-apt install isc-dhcp-server iptables -y
+checkPackage () {
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $1|grep "install ok installed")
+    echo Checking for $1: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+        echo "No $1. Setting up $1."
+        sudo apt-get --yes install $1
+    fi
+}
+
+REQUIRED_PKG="isc-dhcp-server"
+checkPackage $REQUIRED_PKG
+
+REQUIRED_PKG="iptables"
+checkPackage $REQUIRED_PKG
+
+REQUIRED_PKG="iptables-persistent"
+checkPackage $REQUIRED_PKG
 
 echo 'INSTALACIONES COMPLETAS'
 
@@ -22,7 +38,7 @@ cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.original
 ###################################################
 #####---- Configuracion Global DHCPd.conf ----#####
 ###################################################
-echo 'COMIENDO DE LA CONFIGURACION EN /etc/dhcp/dhcpd.conf'
+echo 'COMIENZO DE LA CONFIGURACION EN /etc/dhcp/dhcpd.conf'
 
 echo 'ddns-update-style none;'                      >  /etc/dhcp/dhcpd.conf 
 
@@ -37,7 +53,7 @@ echo ''                                             >> /etc/dhcp/dhcpd.conf
 echo 'subnet 40.12.1.0 netmask 255.255.255.0 {'     >> /etc/dhcp/dhcpd.conf 
 echo '  range 40.12.1.2 40.12.1.254;'               >> /etc/dhcp/dhcpd.conf 
 echo '  option routers 40.12.1.1;'                  >> /etc/dhcp/dhcpd.conf 
-echo '  option domain-name-servers 8.8.8.8;'        >> /etc/dhcp/dhcpd.conf 
+echo '  option domain-name-servers 192.168.1.2;'    >> /etc/dhcp/dhcpd.conf 
 echo '  default-lease-time 172800;'                 >> /etc/dhcp/dhcpd.conf 
 echo '  max-lease-time 259200;'                     >> /etc/dhcp/dhcpd.conf 
 echo '}'                                            >> /etc/dhcp/dhcpd.conf
@@ -48,7 +64,7 @@ echo ''                                             >> /etc/dhcp/dhcpd.conf
 echo 'subnet 192.168.1.0 netmask 255.255.255.0 {'   >> /etc/dhcp/dhcpd.conf 
 echo '  range 192.168.1.2 192.168.1.254;'           >> /etc/dhcp/dhcpd.conf 
 echo '  option routers 192.168.1.1;'                >> /etc/dhcp/dhcpd.conf 
-echo '  option domain-name-servers 8.8.8.8;'        >> /etc/dhcp/dhcpd.conf 
+echo '  option domain-name-servers 192.168.1.2;'    >> /etc/dhcp/dhcpd.conf 
 echo '}'                                            >> /etc/dhcp/dhcpd.conf
 
 echo ''                                             >> /etc/dhcp/dhcpd.conf 
@@ -57,7 +73,7 @@ echo ''                                             >> /etc/dhcp/dhcpd.conf
 echo 'subnet 192.168.2.0 netmask 255.255.255.0 {'   >> /etc/dhcp/dhcpd.conf 
 echo '  range 192.168.2.2 192.168.2.254;'           >> /etc/dhcp/dhcpd.conf 
 echo '  option routers 192.168.2.1;'                >> /etc/dhcp/dhcpd.conf 
-echo '  option domain-name-servers 8.8.8.8;'        >> /etc/dhcp/dhcpd.conf 
+echo '  option domain-name-servers 192.168.1.2;'    >> /etc/dhcp/dhcpd.conf 
 echo '}'                                            >> /etc/dhcp/dhcpd.conf
 
 echo ''                                             >> /etc/dhcp/dhcpd.conf 
@@ -85,7 +101,22 @@ echo 'net.ipv4.ip_forward=1'                        > /etc/sysctl.conf
 ###################################################
 echo 'COMIENZO DE LA CONFIGURACION IPTABLES /etc/default/isc-dhcp-server.conf'
 sleep 2
-sudo iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
+iptables --flush 
+
+iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
+
+iptables -P INPUT DROP # Dropear todos los paquetes de la tabla input
+iptables -P FORWARD DROP # Dropear todos los paquetes de la tabla forward
+iptables -P OUTPUT DROP # Dropear todos los paquetes de la tabla output
+
+iptables -A INPUT -i lo -j ACCEPT  # Permitir todos los paquetes a la red local
+iptables -A OUTPUT -o lo -j ACCEPT  # Permitir todos los paquetes desde la red local
+
+iptables -A FORWARD -p udp -s 192.168.1.0/24 -o ens33 --dport 53 --sport 1024:65535 -j ACCEPT  # Habilitar DNS
+iptables -A FORWARD -p tcp -s 192.168.1.0/24 -o ens33 -m multiport --dport 80,443 -j ACCEPT  # Habilitar HTTP, HTTPS
+
+# Make persistent rule of iptables
+iptables-save                                       > /etc/iptables/rules.v4
 echo 'FIN DE LA CONFIGURACION IPTABLES /etc/default/isc-dhcp-server.conf'
 sleep 2
 
